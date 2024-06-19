60d273f7496c3b59f98dcd620ec1d760821cb11f
ESWARKUMAR2893 <44479009+ESWARKUMAR2893@users.noreply.github.com>
2024-06-19 17:55:32 +0530

    Add files via upload

--- RealProject/file_processing.py
+++ RealProject/file_processing.py
@@ -1,6 +1,8 @@
 import argparse
 import json
 import os
+import pandas as pd
+from datetime import datetime
 
 
 def load_json(file_name):
@@ -22,9 +24,10 @@ def read_database_config(file_name, number):
     :param number: The key in the JSON file for which the config is to be read.
     :return: The type of the database from the configuration.
     """
+    print(f"\nReading {file_name} file ..")
     config = load_json(file_name)
     db_type = config[str(number)]["type"]
-    print("ReadDB config...", db_type)
+    print(f"\n Extracted Database type is {db_type} \n")
     return db_type
 
 
@@ -36,11 +39,14 @@ def read_file_config(file_name, number):
     :param number: The key in the JSON file for which the config is to be read.
     :return: A tuple containing file info and file extension.
     """
+    print(f"\nReading {file_name} file ...")
     config = load_json(file_name)
     file_info = config[str(number)]
-    file_extension = os.path.splitext(file_info)[1][1:]
-    print("Read File Config", file_info)
-    return file_info, file_extension
+    file_name = file_info["file_name"]
+    file_extension = os.path.splitext(file_name)[1][1:]  # Extract extension from file_name
+    delimiter = file_info.get("delimiter", "")  # Get delimiter from file_info, default to empty string if not present
+    print(f"Extracted file name: {file_name}  with Delimiter: '{delimiter}' \n")
+    return file_name, delimiter, file_extension
 
 
 def read_file_output_config(file_name, number):
@@ -51,9 +57,10 @@ def read_file_output_config(file_name, number):
     :param number: The key in the JSON file for which the config is to be read.
     :return: The type of the output from the configuration.
     """
+    print(f"\nReading {file_name} file ..")
     config = load_json(file_name)
     file_type = config[str(number)]
-    print("Type:", file_type)
+    print(f"Extracted File type which we need to convert is {file_type} type \n")
     return file_type
 
 
@@ -65,19 +72,54 @@ def read_api_config(file_name, number):
     :param number: The key in the JSON file for which the config is to be read.
     :return: The name of the API from the configuration.
     """
+    print(f"Reading {file_name} file")
     config = load_json(file_name)
     api_name = config[str(number)]
-    print("Reading API...", api_name)
+    print(f"Extracted API name from file is {api_name} \n")
     return api_name
 
 
 # Define utility functions for each conversion
-def file_to_db():
-    print("Calling File to DB Utility....")
+def file_to_db(source,target):
+    print(f"We are in a function file_to_db to Extract {source[0][0]} and convert to {target[0]} DB\n")
+    print("Work in progress....")
 
 
-def file_to_output():
-    print("Calling File to File Conversion Logic....")
+def transform_data(data_frame):
+    # Adjust the columns dynamically based on the input data
+    required_columns = {'name', 'age', 'date_of_joining', 'id', 'author', 'title', 'genre', 'price', 'publish_date'}
+    columns_to_keep = [col for col in data_frame.columns if col in required_columns]
+    return data_frame[columns_to_keep]
+
+
+def extract_csv(file_path, delimiter):
+    return pd.read_csv(file_path, delimiter=delimiter)
+
+
+def file_to_output(source,target):
+    print(f"We are in a function file_to_output to Extract {source[0][0]} and convert to {target[0]} format")
+    # print("^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    #print(source)
+    # print("Target",target[0])
+    output_format = target[0]
+    filename = source[0][0]
+    delimiter = source[0][1]
+    print("filename is :",filename)
+    print("delimiter is :",delimiter)
+    data = extract_csv(filename, delimiter)
+    data_frame = transform_data(data)
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    filename = f"output_{timestamp}.{output_format}"
+    if output_format == 'csv':
+        data_frame.to_csv(filename, index=False, sep=delimiter)
+    elif output_format == 'txt':
+        data_frame.to_csv(filename, index=False, sep=delimiter)
+    elif output_format == 'json':
+        data_frame.to_json(filename, orient='records', lines=True)
+    else:
+        raise ValueError(f"Unsupported output format: {output_format}")
+
+    print(f"{filename} Created Successfully..")
 
 
 def file_to_api():
@@ -153,6 +195,7 @@ def main():
     source_main = process_file(args.file1, args.num1)
     target_main = process_file(args.file2, args.num2)
     print("Source:", source_main[-1], "Target:", target_main[-1])
+    print(f"\nBy the inputs you passed we need to convert source {source_main[-1]} to target {target_main[-1]} type \n")
 
     source, target = source_main[-1], target_main[-1]
 
@@ -162,14 +205,15 @@ def main():
     else:
         if source == "FILE":
             if target == "DB":
-                file_to_db()
+                file_to_db(source_main,target_main)
             elif target == "OUTPUT":
-                file_to_output()
+                file_to_output(source_main,target_main)
             elif target == "API":
                 file_to_api()
         elif source == "DB":
             if target == "FILE":
-                db_to_file()
+                print("This Combination is not allowed: From DB to FILE ")
+                #db_to_file()
             elif target == "OUTPUT":
                 db_to_output()
             elif target == "API":
